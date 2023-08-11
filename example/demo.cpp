3f9c0584a678f3cec6a69884e16ae7353d196f7e
straykangaroo <straykangaroo>
2023-08-11 16:27:08 +0200

    tabs (demo.cpp)

--- example/demo.cpp
+++ example/demo.cpp
@@ -34,19 +34,19 @@ int main(int argc, char *argv[])
     using namespace argolis;
 
     Parser parser({
-        {'v',"version",Arg_Policy::NO_ARG,version},		/* this option does not want an argument, it will raise an error if one given	*/
+        {'v',"version",Arg_Policy::NO_ARG,version},                 /* this option does not want an argument, it will raise an error if one given   */
 
-        {'h',Arg_Policy::NO_ARG,							/* same	( but option only available in "short form"								*/
-            [](auto,auto)											/* we can safely ignore the arguments to the lambda if they are not needed		*/
+        {'h',Arg_Policy::NO_ARG,                                    /* same ( but option only available in "short form"                             */
+            [](auto,auto)                                           /* we can safely ignore the arguments to the lambda if they are not needed      */
             {
                 usage();
             }
         },
         
-        {'c',"count",Arg_Policy::EXPECT_ARG,				/* this option needs an argument, it will raise an error if none given*/
-            [& count](const auto & /* opt */, auto value)			/* type of opt is argolis::Parser::Opt_Spec, type of value is std::optional<std::string_view>	*/
+        {'c',"count",Arg_Policy::EXPECT_ARG,                        /* this option needs an argument, it will raise an error if none given*/
+            [& count](const auto & /* opt */, auto value)           /* type of opt is argolis::Parser::Opt_Spec, type of value is std::optional<std::string_view>   */
             {
-                assert(value);										// since this option is "NEED_ARG", we expect value to contain an argument
+                assert(value);                                      // since this option is "NEED_ARG", we expect value to contain an argument
 
                 auto res = std::from_chars(value->data(),value->data()+value->size(),count);
                 if( res.ec != std::errc{} || count <= 0 ) {
@@ -57,17 +57,17 @@ int main(int argc, char *argv[])
             }
         },
         
-        {'a',"adjective",Arg_Policy::MAYBE_ARG,			/* this option might get an argument, or not	*/
-            [& adjective](const auto & /* opt */, auto value) {		/* type of opt is argolis::Parser::Opt_Spec, type of value is std::optional<std::string_view>	*/
+        {'a',"adjective",Arg_Policy::MAYBE_ARG,                     /* this option might get an argument, or not    */
+            [& adjective](const auto & /* opt */, auto value) {     /* type of opt is argolis::Parser::Opt_Spec, type of value is std::optional<std::string_view>   */
                 adjective = value.value_or("dear");
             }
         }
     });
-    parser.combi_allowed(true);										// allow options in short form to be combined after a dash: "-ac" is equivalent to "-a -c", not to "--ac"
+    parser.combi_allowed(true);                                     // allow options in short form to be combined after a dash: "-ac" is equivalent to "-a -c", not to "--ac"
 
-    parser.abort_on_error(true);									// abort parsing on the first error
+    parser.abort_on_error(true);                                    // abort parsing on the first error
 
-    parser.on_err([](auto err){										/* type of err is argolis::Parser::Error	*/
+    parser.on_err([](auto err){                                     /* type of err is argolis::Parser::Error        */
         std::cerr << "error with option: " << err.bad_item << "\n";
         std::exit(1);
     });
@@ -76,7 +76,7 @@ int main(int argc, char *argv[])
     parser.on_arg([& count, & adjective](std::string_view arg) {
         for( int i = 0; i < count; i++) {
             std::cout << "Hello, " << adjective << ' ' << arg << '\n';
-        }		
+        }
     });
     
     if( argc < 2 ) {
